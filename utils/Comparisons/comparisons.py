77832087f08499b975f2488636836d6b0bd57040
majchandra <161589972+majchandra@users.noreply.github.com>
2024-04-28 20:20:25 +0100

    Add files via upload

--- utils/Comparisons/comparisons.py
+++ utils/Comparisons/comparisons.py
@@ -9,7 +9,9 @@ import re
 
 
 def skip_header(content):
-    """Find dynamiccly the number of lines to skip in the header of the file."""
+    """
+    Find dynamiccly the number of lines to skip in the header of the file.
+    """
     lines = content.split("\n")
     lines_to_skip = 0
     for line in lines:
@@ -21,7 +23,9 @@ def skip_header(content):
 
 
 def get_header(content):
-    """Get dynamiccly the header of the dataset."""
+    """
+    Get dynamiccly the header of the dataset.
+    """
     lines = content.split("\n")
     line = lines[skip_header(content) - 2]
     header = re.split(r"\s+", line.strip())[1:]
@@ -54,77 +58,107 @@ def download_and_combine_data(Re_tau):
     return df
 
 
-def data_interpolation(df, nb):
+def display_plots(df_PySINDy=None, df_PINNs=None, df_DNS=None, df_DNS_Oden=None, df_RSM=None, df_kw=None,log_scale=None,grid=None):
     """
-    Interpolate the data to have the same number of points for each Reynolds number
+    Plot the predicted values from multiple turbulence models.
+    
     Args:
-        df (pd.DataFrame): The dataframe containing the data
-        nb (int): The number of points to interpolate
+        df_PySINDy (pd.DataFrame): The dataframe containing PySINDy results
+        df_PINNs (pd.DataFrame): The dataframe containing PINNs results
+        df_DNS (pd.DataFrame): The dataframe containing the DNS data
+        df_DNS_Oden (pd.DataFrame): The dataframe containing the DNS data (from Oden)
+        df_RSM (pd.DataFrame): The dataframe containing RSM results
+        df_kw (pd.DataFrame): The dataframe containing k-w results
+        log_scale (bool): If true, use logarithmic scale 
+        grid (bool): If true, display the grid on the plots
 
     Returns:
-        pd.DataFrame: The interpolated dataframe    """
-    interval = np.linspace(df['y^+'].min(), df['y^+'].max(), num=nb)
-    interpolated_columns = {}
-    for column in df.columns:
-        if column != 'y^+':
-            f = interp1d(df['y^+'], df[column], kind='linear')
-            interpolated_columns[column] = f(interval)
-    df = pd.DataFrame(interpolated_columns)
-    df['y^+'] = interval
-    return df
-
-def display_plots(df_PySINDy=None, df_PINNs=None, df_DNS=None, df_DNS_Oden=None, df_RSM=None, df_kw=None,log_scale=None,grid=None):
+        None
+    """
+    # Define the names of the columns to be plotted
     col = ["U","u'u'","v'v'","w'w'","u'v'"]
+
     for i in range(5):
         plt.figure(figsize=(12, 6))
+
+        # For U, include data from the k-w model  
         if col[i] == "U":
             if df_kw is not None:
                 kw = df_kw[col[i]]
-                y = df_kw["y^+"]
+                y_kw = df_kw["y^+"]
+        
+        # Retrieve data from the DNS model (Oden) if available
         if df_DNS_Oden is not None:
             dns_oden = df_DNS_Oden[col[i]]
-            y = df_DNS_Oden["y^+"]
+            y_dns_oden = df_DNS_Oden["y^+"]
+        
+        # Retrieve data from the DNS model if available
+        if df_DNS is not None:
+            dns = df_DNS[col[i]]
+            y_dns = df_DNS["y^+"]
+        
+        # Except for w'w', include data from the RSM model
         if col[i] != "w'w'":
             if df_RSM is not None:
                 rsm = df_RSM[col[i]]
-                y = df_RSM["y^+"]
+                y_rsm = df_RSM["y^+"]
+        
+        # Retrieve data from the PySINDy model (Oden) if available
         if df_PySINDy is not None:
             pysindy = df_PySINDy[col[i]]
-            y = df_PySINDy["y^+"]
+            y_pysindy = df_PySINDy["y^+"]
+        
+        # Retrieve data from the PINNs model (Oden) if available
         if df_PINNs is not None:
             pinns = df_PINNs[f"{col[i]}_pred"]
-            y = df_PINNs["y^+"]
+            y_pinns = df_PINNs["y^+"]
+        
+
         if col[i] == "u'v'":
             if df_DNS_Oden is not None:
                 dns_oden = -1 * df_DNS_Oden[col[i]] 
+            if df_DNS is not None:
+                dns = -1 * df_DNS[col[i]] 
             if df_PySINDy is not None:
                 pysindy = -1 * df_PySINDy[col[i]]
             if df_RSM is not None:
                 rsm = -1 * df_RSM[col[i]]
             if df_PINNs is not None:
                 pinns = -1 * df_PINNs[f"{col[i]}_pred"]
+        
+        # Plot data for each model if available
         if df_PySINDy is not None:
-            plt.plot(y.values, pysindy.values,linestyle="-",color="green",label = f"{col[i]} (PySINDy)")
+            plt.plot(y_pysindy.values, pysindy.values,linestyle="-",color="green",label = f"{col[i]} (PySINDy)")
         if df_PINNs is not None:
-            plt.plot(y.values, pinns.values,linestyle="-",color="blue",label = f"{col[i]} (PINNs)")
+            plt.plot(y_pinns.values, pinns.values,linestyle="-",color="blue",label = f"{col[i]} (PINNs)")
         if df_DNS_Oden is not None:
-            plt.plot(y.values, dns_oden.values,linestyle="--",color="red" ,label=f"{col[i]} (DNS Oden)")
-        if df_RSM is not None:
-            plt.plot(y.values, rsm.values,linestyle="-",color="black" ,label=f"{col[i]} (RSM)")
+            plt.plot(y_dns_oden.values, dns_oden.values,linestyle="--",color="red" ,label=f"{col[i]} (DNS Oden)")
+        if df_DNS is not None:
+            plt.plot(y_dns.values, dns.values,linestyle="--",color="yellow" ,label=f"{col[i]} (DNS)")
+        if col[i] != "w'w'":
+            if df_RSM is not None:
+                plt.plot(y_rsm.values, rsm.values,linestyle="-",color="black" ,label=f"{col[i]} (RSM)")
         if col[i] == "U":
             if df_kw is not None:
-                plt.plot(y.values, kw.values,linestyle="-",color="orange" ,label=f"{col[i]} (k–ω)")
+                plt.plot(y_kw.values, kw.values,linestyle="-",color="orange" ,label=f"{col[i]} (k–ω)")
+        
+        # Set logarithmic scale if enabled
         if log_scale:
             plt.xscale('log')
+        # Display the grid on the plots if enabled
+        if grid:    
+            plt.grid(True, which="both", linestyle='--', linewidth=0.5)
+
         plt.xlabel('y^+')
+
         if col[i] == "u'v'":
             plt.ylabel(f"- {col[i]}")
             plt.title(f"- {col[i]} as a function of y^+")
         else:
             plt.ylabel(col[i])
             plt.title(f"{col[i]} as a function of y^+")
-        if grid:    
-            plt.grid(True, which="both", linestyle='--', linewidth=0.5)
+
+
         plt.legend()
         st.pyplot(plt)
         plt.close()
--- utils/Comparisons/comparisons_streamlit.py
+++ utils/Comparisons/comparisons_streamlit.py
@@ -4,36 +4,52 @@ import streamlit as st
 import pandas as pd
 from utils.Comparisons.comparisons import *
 
-columns = ["y^+", "U", "u'u'", "v'v'", "w'w'", "u'v'"]
-columns1 = ["y^+", "U_pred", "u'u'_pred", "v'v'_pred", "w'w'_pred", "u'v'_pred"]
-columns2 = ["y^+","U","uu (y^+) RST","vv (y^+)","uv RST (y^+)"]
-columns3 = ["y^+","U k-w"]
+# Define the relevant columns for the PySINDy model 
+columns_pysindy = ["y^+", "U", "u'u'", "v'v'", "w'w'", "u'v'"]
+# Define the relevant columns for the PINNs model 
+columns_pinns = ["y^+", "U_pred", "u'u'_pred", "v'v'_pred", "w'w'_pred", "u'v'_pred"]
+# Define the relevant columns for the RSM  
+columns_rsm = ["y^+","U","uu (y^+) RST","vv (y^+)","uv RST (y^+)"]
+# Define the relevant columns for the kw model 
+columns_kw = ["y^+","U k-w"]
 
 
 def app():
-    #st.image(logo_path, width=100)
     if "comparison" not in st.session_state:
         st.session_state.comparison = False
+    
     st.write("# Turbulence Modelling Predictor")
     st.markdown('<p style="font-size: 15px; font-style: italic;"> ~Developed by Group 2 Cranfield CO-OP</p>',unsafe_allow_html=True) 
+    
+    # Selection of models to compare
     options = ['DNS (Oden)', 'DNS', 'PySINDy', 'PINNs','RSM','k–ω']
     selected_models = st.multiselect("Please select the models to be compared:",options)
+    
+    # Check whether any models have been selected for the comparison
     if len(selected_models) == 0:
         st.session_state.comparison = False
         return None 
         
     if "PySINDy" in selected_models:
         st.header("PySINDy")
+
+        # File uploader for users to upload a CSV file for PySINDy
         uploaded_file_pysindy = st.file_uploader("\n\n**Please upload the CSV below.**", type=['csv'], key="PySINDy")
+
+        # Check if a csv file has been uploaded
         if uploaded_file_pysindy is not None:
+            
             df_PySINDy = pd.read_csv(uploaded_file_pysindy)
-            if all(col in df_PySINDy.columns for col in columns):
-                df_PySINDy = df_PySINDy[columns]
+
+            # Check if the uploaded CSV file contains all the necessary columns
+            if all(col in df_PySINDy.columns for col in columns_pysindy):
+                df_PySINDy = df_PySINDy[columns_pysindy]
                 st.success("File successfully uploaded!")
             else:
                 st.error("Please upload a csv file with the columns y^+, U, u'u', v'v', w'w'and u'v' to proceed.")
                 return None
         else:
+            # Error message if no file is uploaded
             st.error("Please upload a csv file to proceed.")
             return None
     else:
@@ -41,16 +57,24 @@ def app():
     
     if "PINNs" in selected_models:
         st.header("PINNs")
+        
+        # File uploader for users to upload a CSV file for PINNs
         uploaded_file_pinns = st.file_uploader("\n\n**Please upload the CSV below.**", type=['csv'], key="PINNs")
+        
+        # Check if a csv file has been uploaded
         if uploaded_file_pinns is not None:
+            
             df_PINNs = pd.read_csv(uploaded_file_pinns)
-            if all(col in df_PINNs.columns for col in columns1):
-                df_PINNs = df_PINNs[columns1]
+
+            # Check if the uploaded CSV file contains all the necessary columns
+            if all(col in df_PINNs.columns for col in columns_pinns):
+                df_PINNs = df_PINNs[columns_pinns]
                 st.success("File successfully uploaded!")
             else:
                 st.error("Please upload a csv file with the columns y^+, U_pred, u'u'_pred, v'v'_pred, w'w'_pred and u'v'_pred to proceed.")
                 return None
         else:
+            # Error message if no file is uploaded
             st.error("Please upload a csv file to proceed.")
             return None
     else:
@@ -58,6 +82,7 @@ def app():
 
     if "DNS (Oden)" in selected_models:
         st.header("DNS (Oden)")
+        # Select a Reynolds number for the extraction of the data present on the Oden site associated with this selected Reynolds number
         selected_re = st.selectbox("Select a Reynolds Number:", ["5200", "2000", "1000", "550", "180"])
         df_DNS_oden = download_and_combine_data(selected_re)
     else:
@@ -65,16 +90,24 @@ def app():
 
     if "DNS" in selected_models:
         st.header("DNS")
+
+        # File uploader for users to upload a CSV file for DNS
         uploaded_file_dns = st.file_uploader("\n\n**Please upload the CSV below.**", type=['csv'], key="DNS")
+        
+        # Check if a csv file has been uploaded
         if uploaded_file_dns is not None:
+            
             df_DNS = pd.read_csv(uploaded_file_dns)
-            if all(col in df_DNS.columns for col in columns):
-                df_DNS = df_DNS[columns]
+
+            # Check if the uploaded CSV file contains all the necessary columns
+            if all(col in df_DNS.columns for col in columns_pysindy):
+                df_DNS = df_DNS[columns_pysindy]
                 st.success("File successfully uploaded!")
             else:
                 st.error("Please upload a csv file with the columns y^+, U, u'u', v'v', w'w'and u'v' to proceed.")
                 return None
         else:
+            # Error message if no file is uploaded
             st.error("Please upload a csv file to proceed.")
             return None
     else:
@@ -82,10 +115,17 @@ def app():
 
     if "RSM" in selected_models:
         st.header("RSM")
+
+        # File uploader for users to upload a CSV file for RSM
         uploaded_file_rsm = st.file_uploader("\n\n**Please upload the CSV below.**", type=['csv'], key="RSM")
+        
+        # Check if a csv file has been uploaded
         if uploaded_file_rsm is not None:
+            
             df_RSM = pd.read_csv(uploaded_file_rsm)
-            if all(col in df_RSM.columns for col in columns2):
+
+            # Check if the uploaded CSV file contains all the necessary columns
+            if all(col in df_RSM.columns for col in columns_rsm):
                 st.success("File successfully uploaded!")
                 df_RSM["u'u'"] = df_RSM["uu (y^+) RST"]
                 df_RSM["v'v'"] = df_RSM["vv (y^+)"]
@@ -95,6 +135,7 @@ def app():
                 st.error("Please upload a csv file with the columns y^+, uu (y^+) RST, vv (y^+), uv RST (y^+) to proceed.")
                 return None
         else:
+            # Error message if no file is uploaded
             st.error("Please upload a csv file to proceed.")
             return None
     else:
@@ -102,10 +143,17 @@ def app():
 
     if "k–ω" in selected_models:
         st.header("k–ω")
+        
+        # File uploader for users to upload a CSV file for k-w 
         uploaded_file_kw = st.file_uploader("\n\n**Please upload the CSV below.**", type=['csv'], key="kw")
+        
+        # Check if a csv file has been uploaded
         if uploaded_file_kw is not None:
+            
             df_kw = pd.read_csv(uploaded_file_kw)
-            if all(col in df_kw.columns for col in columns3):
+
+            # Check if the uploaded CSV file contains all the necessary columns
+            if all(col in df_kw.columns for col in columns_kw):
                 st.success("File successfully uploaded!")
                 df_kw["U"] = df_kw["U k-w"]
                 df_kw = df_kw[["y^+","U"]]
@@ -113,28 +161,34 @@ def app():
                 st.error("Please upload a csv file with the columns y^+ and U k-w to proceed.")
                 return None
         else:
+            # Error message if no file is uploaded
             st.error("Please upload a csv file to proceed.")
             return None
     else:
         df_kw = None
     
+    # Run the comparison of the selected models
     if st.button("Comparison of selected models"):
         st.session_state.comparison = True
 
     if st.session_state.comparison:
+        # List containing the dataframes of different models
         dfs = [df_PySINDy, df_PINNs, df_DNS, df_DNS_oden, df_RSM, df_kw]
+
+        # Initialise a list to store possibly interpolated dataframes
         df = [None] * len(dfs)
+
+        # Iterate on the dataframes associated with the models
         for i in range (len(dfs)):
+            # Checks whether the dataframe associated with this model has been uploaded
             if dfs[i] is not None:
                 df[i] = dfs[i]
-        if len(dfs) > 1: 
-            nb =  max(len(dataset) for dataset in df if dataset is not None)
-            for i in range(len(dfs)):
-                if df[i] is not None and len(df[i]) < nb:
-                    df[i] = data_interpolation(df[i],nb)
-            log_scale = st.checkbox("Use logarithmic scale")
-            show_grid = st.checkbox("Show grid on the plots")
-            display_plots(df[0], df[1], df[2], df[3], df[4], df[5],log_scale,show_grid)
-        else:
-            st.error("Please upload more than one valid dataset for comparison.")
-       
+
+        log_scale = st.checkbox("Use logarithmic scale")
+
+        # Allow the user to choose whether to use a grid for the plots
+        show_grid = st.checkbox("Show grid on the plots")
+
+        # Display the plots for the uploaded models 
+        display_plots(df[0], df[1], df[2], df[3], df[4], df[5],log_scale,show_grid)
+
